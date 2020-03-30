d0f634710d028c856dba082ed56dcf98ef60b939
ayushman264 <ayushman264@gmail.com>
2020-03-30 16:15:57 -0700

    Updated code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Software-Security-CMPE279
+
+Assignment 1
+1. gcc -o server server.c
+2. sudo ./server
+3. gcc -o client client.c
+4. sudo ./client
--- server.c
+++ server.c
@@ -51,9 +51,23 @@ int main(int argc, char const *argv[])
         perror("accept"); 
         exit(EXIT_FAILURE); 
     } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
+
+    int newproc=fork();
+    //child process
+    if(newproc==0){
+	//setuid to nobody user
+	int uid=setuid(65534);
+	valread = read( new_socket , buffer, 1024); 
+    	printf("%s\n",buffer ); 
+    	send(new_socket , hello , strlen(hello) , 0 ); 
+    	printf("Hello message sent\n"); 
+    }
+    //parent process
+    else if(newproc>0){
+	wait(NULL);
+    }
+    else{
+	printf("Fork failed");
+    }
     return 0; 
 } 
